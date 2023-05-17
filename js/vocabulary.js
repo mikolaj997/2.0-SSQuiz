ebfb9e7ca49c3f2e1d496da54316b8db018e7640
mikolaj.orzol@onet.pl <m.orzol.306@studms.ug.edu.pl>
2023-05-17 19:45:48 +0200

    first steps with vue app

---
+++ js/vocabulary.js
@@ -0,0 +1,5 @@
+[
+    { Id: 1, word: "acid",  definition: "kwas", Level: 4, ExtendedCategory: "pollution" , PrecizedCategory: "substancja"},
+    { Id: 2, word: "petrol",  definition: "benzyna", Level: 4,ExtendedCategory: "pollution" , PrecizedCategory: "substancja"},
+    { Id: 3,word: "nitrogen",  definition: "substancja", Level: 4,ExtendedCategory: "pollution" , PrecizedCategory: "pollution" }
+]

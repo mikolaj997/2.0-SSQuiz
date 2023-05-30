fd01a19ffd5ce08d1fe8935584d732e8b81926ea
mikolaj.orzol@onet.pl <m.orzol.306@studms.ug.edu.pl>
2023-05-30 14:52:05 +0200

    working modified buttons with its html and style

--- js/main.js
+++ js/main.js
@@ -291,6 +291,7 @@ createApp({
 
             // Pozostałe pytania i odpowiedzi
             showPopup: false,
+            showPopup2: false,
             currentSlide: 0,
             correctAnswers: 0,
             showResult: false,
@@ -309,6 +310,9 @@ createApp({
         //     },
         togglePopup() {
             this.showPopup = !this.showPopup;
+        },
+        togglePopup2() {
+            this.showPopup2 = !this.showPopup2;
         },
 
         checkIfCorrect(definition) {
